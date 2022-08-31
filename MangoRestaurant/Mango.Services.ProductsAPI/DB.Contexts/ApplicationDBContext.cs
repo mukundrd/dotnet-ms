34fd149187f6a2c0e3193bed7beec69f33141d1f
Mukund Desai <muks.dev@trayis.com>
2022-08-31 18:23:22 +0530

    adding MangoRestaurant base code

---
+++ MangoRestaurant/Mango.Services.ProductsAPI/DB.Contexts/ApplicationDBContext.cs
@@ -0,0 +1,27 @@
+﻿using Mango.Services.ProductsAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Mango.Services.ProductsAPI.DB.Contexts
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
